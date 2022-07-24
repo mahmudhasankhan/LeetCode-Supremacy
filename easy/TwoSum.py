9379ef614e6c53de6bce483d18d3678fd664a398
Khanzaadeh <khanzadeh008@gmail.com>
2022-07-24 23:26:53 +0600

    included brute force method of two sum

--- easy/TwoSum.py
+++ easy/TwoSum.py
@@ -1,3 +1,4 @@
+# Optimized hashmap solution
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -5,11 +6,29 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        map = {}  # val : index
-        
+        map = {}  # val : index     
         for i, n in enumerate(nums):
             diff = target - n
             if diff in map:
                 return [map[diff], i]
             map[n] = i
+            
+
+#  Brute Force           
+class Brute_Force(object):
     
+    def twoSumBrute(self, nums, target):
+        
+        res = []
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    res.append(i)
+                    res.append(j) 
+        return res
+
+
+a = Brute_Force()
+lst = [3, 3]
+target = 6
+print(a.twoSumBrute(lst, target))
