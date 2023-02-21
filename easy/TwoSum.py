c3818e268779692d0364b823d046ec525759ee28
Khanzaadeh <khanzadeh008@gmail.com>
2023-02-21 12:31:19 +0600

    Modified two-sum

--- easy/TwoSum.py
+++ easy/TwoSum.py
@@ -1,34 +1,27 @@
 # Optimized hashmap solution
 class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        map = {}  # val : index     
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in map:
-                return [map[diff], i]
-            map[n] = i
-            
+    def twoSum(self, nums: list, target: int) -> list:
+        hashmap = {}  # val : index
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            hashmap[num] = i
 
-#  Brute Force           
+
+#  Brute Force
 class Brute_Force(object):
-    
-    def twoSumBrute(self, nums, target):
-        
+    def twoSumBrute(self, nums: list, target: int) -> list:
+
         res = []
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
+            for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     res.append(i)
-                    res.append(j) 
+                    res.append(j)
         return res
 
 
 a = Brute_Force()
 lst = [3, 3]
-target = 6
-print(a.twoSumBrute(lst, target))
+print(a.twoSumBrute(lst, 6))
