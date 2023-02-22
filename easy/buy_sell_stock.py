4fe01332207795f0e70422c87f2df380da2a6f5a
Khanzaadeh <khanzadeh008@gmail.com>
2023-02-22 08:55:20 +0600

    wrote my own code -> buy sell stock problem

--- easy/buy_sell_stock.py
+++ easy/buy_sell_stock.py
@@ -1,17 +1,19 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        left_pointer, right_pointer = 0, 1  # left = buy. right = sell
+        # using the two pointers technique to solve this problem.
+        left_pointer, right_pointer = 0, 1
         max_profit = 0
 
         while right_pointer < len(prices):
-            # profitable ?
             if prices[left_pointer] < prices[right_pointer]:
-                profit = prices[right_pointer] - prices[left_pointer]
-                max_profit = max(max_profit, profit)
+                max_profit = max(
+                    max_profit, prices[right_pointer] - prices[left_pointer]
+                )
             else:
                 left_pointer = right_pointer
 
             right_pointer += 1
+
         return max_profit
 
 
