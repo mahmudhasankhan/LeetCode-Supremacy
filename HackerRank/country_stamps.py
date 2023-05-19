0e15ccdbfa4ee45bd7f3f7d7bc61ec61a2fcfad6
Khanzaadeh <khanzadeh008@gmail.com>
2023-05-19 16:38:56 +0600

    country stamps

---
+++ HackerRank/country_stamps.py
@@ -0,0 +1,11 @@
+
+def main():
+    iterations = int(input())
+    stamps = set()
+    for _ in range(iterations):
+        stamps.add(input())
+    print(len(stamps))
+
+
+if __name__ == "__main__":
+    main()
