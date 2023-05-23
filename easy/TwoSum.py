b42886c5eaf47f436c121d99cbb22d3f5d9c3701
Khanzaadeh <khanzadeh008@gmail.com>
2023-05-23 06:03:01 +0600

    two sum

--- easy/TwoSum.py
+++ easy/TwoSum.py
@@ -13,13 +13,10 @@ class Solution(object):
 class Brute_Force(object):
     def twoSumBrute(self, nums: list, target: int) -> list:
 
-        res = []
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    res.append(i)
-                    res.append(j)
-        return res
+                    return [i, j]
 
 
 a = Brute_Force()
