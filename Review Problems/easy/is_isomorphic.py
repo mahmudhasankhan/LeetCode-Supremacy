258b1224ce580a60c5e93c2b536c6f31a37e6878
mahmudhasankhan <khanzadeh008@gmail.com>
2023-09-05 11:29:09 +0600

    Is_ismorphic

---
+++ Review Problems/easy/is_isomorphic.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        mapST, mapTS = {}, {}
+        for c1, c2 in zip(s, t):
+            if (c1 in mapST and mapST[c1] != c2 or
+                    (c2 in mapTS and mapTS[c2] != c1)):
+                return False
+            mapST[c1] = c2
+            mapTS[c2] = c1
+        return True
+
+
+def main():
+    print(Solution().isIsomorphic("egg", "add"))
+    print(Solution().isIsomorphic("foo", "bar"))
+    print(Solution().isIsomorphic("paper", "title"))
+
+
+if __name__ == "__main__":
+    main()
