4cfa397698c265dc45f297e25e836d2b1710d767
mahmudhasankhan <khanzadeh008@gmail.com>
2023-10-03 12:40:02 +0600

    Average Selling Price

---
+++ SQL/SQL 50/Basic Aggregate Functions/avg_selling_price.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+SELECT P.product_id
+, ISNULL(ROUND(CAST(SUM(P.price * U.units) AS DECIMAL(10,2)) / CAST(SUM(U.units) AS DECIMAL(10,2)), 2), 0) AS average_price
+FROM Prices AS P
+LEFT JOIN UnitsSold AS U
+ON P.product_id = U.product_id
+AND U.purchase_date BETWEEN P.start_date AND P.end_date
+GROUP BY P.product_id;
