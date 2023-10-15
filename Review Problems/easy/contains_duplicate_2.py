3583c77f5083f830b4183a2118d2c360f3b5baee
mahmudhasankhan <khanzadeh008@gmail.com>
2023-10-15 15:56:56 +0600

    Review Problems: Contains Duplicate (ii), my code is too slow

--- Review Problems/easy/contains_duplicate_2.py
+++ Review Problems/easy/contains_duplicate_2.py
@@ -2,12 +2,25 @@ from typing import List
 
 
 class Solution:
+    def too_slow_containsNearbyDuplicate(self,
+                                         nums: List[int],
+                                         k: int) -> bool:
+        for i in range(len(nums)):
+            j = i + 1
+            while j < len(nums):
+                if nums[i] == nums[j] and abs(i-j) <= k:
+                    return True
+                j += 1
+        return False
+
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         pass
 
 
 def main():
-    pass
+    print(Solution().containsNearbyDuplicate([1, 2, 3, 1], 3))
+    print(Solution().containsNearbyDuplicate([1, 0, 1, 1], 1))
+    print(Solution().containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2))
 
 
 if __name__ == "__main__":
