ec838fe3ddbd55dc60137699f022b6490b829895
mahmudhasankhan <khanzadeh008@gmail.com>
2024-05-12 18:16:06 +0600

    challenges.sql

--- SQL/hackerrank/sql (intermediate)/challenges.sql
+++ SQL/hackerrank/sql (intermediate)/challenges.sql
@@ -1,3 +1,7 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
 DECLARE @max_val = (
 SELECT
 TOP 1 count(c.challenge_id) 
@@ -11,11 +15,7 @@ GROUP BY h.hacker_id, h.name
 ORDER BY count(c.challenge_id) desc
 )
 
-SELECT 
-    hacker_id,
-    name,
-    challenges_created
-(SELECT
+WITH temp as (SELECT
 h.hacker_id, h.name, count(c.challenge_id) as challenges_created
 FROM
     Hackers h
@@ -24,7 +24,20 @@ JOIN
 ON 
     h.hacker_id = c.hacker_id
 GROUP BY h.hacker_id, h.name
-ORDER BY count(c.challenge_id) desc, hacker_id
-) d
-GROUP BY hacker_id, name 
-HAVING count(challenges_created) > 2
+ORDER BY count(c.challenge_id) desc, hacker_id );
+
+
+DELETE FROM TEMP
+hacker_id,
+name,
+challenges_created
+from temp
+group by hacker_id, name
+having count(challenges_created) > 2
+and count(challenges_created) < @max_val
+
+
+
+SELECT * FROM
+temp
+order by challenges_created desc, hacker_id
